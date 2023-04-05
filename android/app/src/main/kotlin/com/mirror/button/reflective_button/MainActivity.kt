2edb6adcfc35d21c37c2634d5b2477ca0a8bfb6c
Olusesi Barry <bolexido192@gmail.com>
2023-04-05 15:28:45 +0100

    built reflective button

---
+++ android/app/src/main/kotlin/com/mirror/button/reflective_button/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mirror.button.reflective_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/reflective/button/reflective_button/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.reflective.button.reflective_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
