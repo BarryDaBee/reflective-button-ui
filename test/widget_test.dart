f135b3cfda85015c96914640de0f2df4c589a5d7
Olusesi Barry <bolexido192@gmail.com>
2023-04-05 15:34:28 +0100

    Code cleanup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:reflective_button/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ReflectiveUIApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
